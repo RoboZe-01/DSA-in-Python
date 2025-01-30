e7bd3652a60cb2bb86e24ccde2add1530fb1675e
RoboZe <roboticsxzero@gmail.com>
2025-01-30 13:10:25 +0530

    First file added

---
+++ Complexities/Time complexity/Problem1.py
@@ -0,0 +1,29 @@
+
+
+
+
+# in this 
+
+
+l = [1,2,3,4,5]
+sum = 0
+for i in l :
+    sum = sum +i       # Simple program to add the numbers in array 
+print(sum)
+
+# Finding product of the numbers 
+
+product = 1
+for j in l :
+    product = product*i
+print(f'Product of the array is {product}')    
+
+
+
+# Now here we have to loops but now nested loop
+# As we know , time complex of single loop = n
+# For two loops  = 2n
+
+# THus it has linear time complexity 
+
+
