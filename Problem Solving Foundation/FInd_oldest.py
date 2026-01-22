8d238d75838d5d5b4015249936ddfdaf0fa4bf50
RoboZe <roboticsxzero@gmail.com>
2026-01-22 22:45:33 +0530

    First problem completed

--- Problem Solving Foundation/FInd_oldest.py
+++ Problem Solving Foundation/FInd_oldest.py
@@ -7,19 +7,37 @@
 
 # Using hard coded values 
 
-age_1 = input("Enter the age of person 1 : ")
-age_2 = input("Enter the age of person 2 : ")     # initialising the values 
-age_3 = input("Enter the age of person 3 : ")
+# age_1 = input("Enter the age of person 1 : ")
+# age_2 = input("Enter the age of person 2 : ")     # initialising the values 
+# age_3 = input("Enter the age of person 3 : ")
 
-if age_1 > age_2 and age_1 >age_3:
-    print("Person 1 is oldest : ", age_1)
-elif age_2 > age_1 and age_2 > age_3:
-    print("second person is oldest :", age_2)  
-else:
-    print("Third person is oldest : ", age_3)
+# if age_1 > age_2 and age_1 >age_3:
+#     print("Person 1 is oldest : ", age_1)
+# elif age_2 > age_1 and age_2 > age_3:
+#     print("second person is oldest :", age_2)  
+# else:
+#     print("Third person is oldest : ", age_3)
   
 
                      ### Method 2 ; Using a function ### 
 
-def find_oldest (age_1,age_2,age_3):
-                         
+# Function definition
+
+def find_oldest ():
+
+    # Taking input from the user
+    age_1 = int(input("Enter the age of person 1 : "))
+    age_2 = int(input("Enter the age of person 2 : "))
+    age_3 = int(input("Enter the age of person 3 : "))
+   
+   # Condition for finding the oldest age
+    if age_1 > age_2 and age_1 >age_3:
+     print("Person 1 is oldest : ", age_1)
+    elif age_2 > age_1 and age_2 > age_3:
+        print("second person is oldest :", age_2)  
+    else:
+        print("Third person is oldest : ", age_3)
+
+#Calling the function 
+
+find_oldest()    
