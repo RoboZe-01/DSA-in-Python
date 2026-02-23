04c1439c9f7464fc88a3f8e049ed68597cb1d3e4
RoboZe <roboticsxzero@gmail.com>
2026-02-23 12:58:05 +0530

    Learnt pseudocode

---
+++ Fundamentals of DSA/pseudoCode.py
@@ -0,0 +1,20 @@
+
+# pseudo code is a simple representation of how our code will work / simple plan in any plain language 
+# It is  used to show how the flow of our code is 
+# No syntax is needed , you can write this however we like 
+
+
+
+# Example : Write a program to add two numbers 
+
+"""
+we will take input from the users using int(input()) two numbers num1 and num2
+
+after that can make a function which will return addition of both 
+
+then we will call this function adn give input 
+and print the addition  
+
+
+
+"""
