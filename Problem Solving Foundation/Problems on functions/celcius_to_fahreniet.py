6e40145bf3def4f88c8ad53258888268730d7e11
RoboZe <roboticsxzero@gmail.com>
2026-03-04 21:52:22 +0530

    Solved simple problems on functions

---
+++ Problem Solving Foundation/Problems on functions/celcius_to_fahreniet.py
@@ -0,0 +1,16 @@
+def celsius_to_fahrenheit(C):
+    """
+    Function to convert temperature from Celsius to Fahrenheit.
+    
+    Parameters:
+    C (float): The temperature in Celsius.
+    
+    Returns:
+    float: The temperature in Fahrenheit.
+    """
+    # Your code here
+    new_temp = (C*9/5)+32
+    return new_temp
+    
+celsius_to_fahrenheit(5)    
+    
