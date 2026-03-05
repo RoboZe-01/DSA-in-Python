e62a7f6f66bab3e073b11d1867c1abb315243550
RoboZe-01 <roboticsxzero@gmail.com>
2026-03-05 12:37:02 +0530

    Binary search implementation

---
+++ Fundamentals of DSA/Searching and Sorting algo/binary_search.py
@@ -0,0 +1,14 @@
+
+def binary_search(arr,target):
+    size = len(arr)
+    end = size-1
+    while 
+
+
+
+
+
+sorted_list = [10,23,35,45,50,70,85]
+target=50
+
+result= binary_search(sorted_list,target)
