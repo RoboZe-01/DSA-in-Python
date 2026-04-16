631e329de7af3bb953f1b120791d088c2c5c8eb7
RoboZe <roboticsxzero@gmail.com>
2026-04-16 16:41:38 +0530

    Hashmaps with projects

---
+++ Problem Solving Foundation/hashmaps problems/problem1.py
@@ -0,0 +1,8 @@
+# Problem statement : 
+
+"""
+Code a program which checks if someone is on the list or not , and what is there status 
+
+if some is on the list then greet them and aknowledge there status 
+else , let them know that they are not on the list
+"""
