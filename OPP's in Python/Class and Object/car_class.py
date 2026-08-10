acb9de5aea7dfa09e0a24e80f7eb129d92352327
RoboZe <roboticsxzero@gmail.com>
2026-08-10 23:34:19 +0530

    deep dive in class

---
+++ OPP's in Python/Class and Object/car_class.py
@@ -0,0 +1,19 @@
+# Creating car class
+
+
+class Car :
+    a = 12  # Attribute
+
+    def hello():
+        print("How are you")
+
+
+
+# You can access attributes and methods after accessing the class
+
+print(Car.a)    # Accesing attributes 
+
+
+Car.hello()    # accessing methods
+
+
---
+++ OPP's in Python/Class and Object/object.py
@@ -0,0 +1,17 @@
+class Bags:
+    name = "NotYourCollege"
+
+    def details(self):
+        print("Hello this is a company who creates bag")
+
+
+
+# Object creation 
+
+reebok = Bags()
+
+# Object has all the power of class ( without encapsulation )
+print(Bags.name)
+
+print(reebok.name)
+
